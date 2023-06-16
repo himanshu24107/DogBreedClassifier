05cb57176af6f73a05b9b973e089c8629271cff3
Himanshu Dubey <112683056+himanshu24107@users.noreply.github.com>
2023-06-16 10:32:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import numpy as np
 
 
 model = tf.keras.models.load_model('keras_model.h5')
-
+st.text("Available Dog breed: American Hunting Dog, Cardigen, Poodle, Maxican Hairless, Dingo, Dhole")
 def preprocess_image(image):
     img = image.resize((224, 224))  # Resize the image to match the input size of the model
     img = np.array(img)  # Convert the image to a numpy array
@@ -27,7 +27,7 @@ def main():
         # Load the class labels or names
         # Replace with your own list of dog breed names
         class_names = ["American Hunting Dog","Cardigen", "Poodle", "Maxican Hairless", "Dingo", "Dhole"]
-
+        
         st.write("Prediction:", class_names[predicted_class])
 
 
