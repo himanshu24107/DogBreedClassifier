d9455a64564c4d6c0c98370d083efe2201f02c06
Himanshu Dubey <112683056+himanshu24107@users.noreply.github.com>
2023-06-16 10:00:10 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,34 @@
+import streamlit as st
+import tensorflow as tf
+from PIL import Image
+import numpy as np
+
+
+model = tf.keras.models.load_model('keras_model.h5')
+
+def preprocess_image(image):
+    img = image.resize((224, 224))  # Resize the image to match the input size of the model
+    img = np.array(img)  # Convert the image to a numpy array
+    img = img / 255.0  # Normalize the pixel values
+    img = np.expand_dims(img, axis=0)  # Add an extra dimension for the batch
+    return img
+
+def main():
+    st.title("Dog Breed Classifier")
+    uploaded_image = st.file_uploader("Choose an image", type=["jpg", "jpeg", "png"])
+
+    if uploaded_image is not None:
+        image = Image.open(uploaded_image)
+        st.image(image, caption='Uploaded Image', use_column_width=True)
+        processed_image = preprocess_image(image)
+        prediction = model.predict(processed_image)
+        predicted_class = np.argmax(prediction)
+
+        # Load the class labels or names
+        # Replace with your own list of dog breed names
+        class_names = ["American Hunting Dog","Cardigen", "Poodle", "Maxican Hairless", "Dingo", "Dhole"]
+
+        st.write("Prediction:", class_names[predicted_class])
+
+
+main()
